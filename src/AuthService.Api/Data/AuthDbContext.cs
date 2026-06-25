34259d67e50897f723cbc843121404f0f5d69750
Carlos Quintero <72052457+CarlosQuinteroC@users.noreply.github.com>
2026-06-24 21:25:12 -0400

    feat: configure database context and connection string for PostgreSQL

---
+++ src/AuthService.Api/Data/AuthDbContext.cs
@@ -0,0 +1,23 @@
+﻿using AuthService.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthService.Api.Data;
+
+public class AuthDbContext : DbContext
+{
+    public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+    {
+        
+    }
+    
+    public DbSet<User> Users { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<UserRole> UserRoles { get; set; }
+    public DbSet<RefreshToken> RefreshTokens { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UserRole>()
+            .HasKey(ur => new { ur.UserId, ur.RoleId });
+    }
+}
--- src/AuthService.Api/Program.cs
+++ src/AuthService.Api/Program.cs
@@ -1,8 +1,16 @@
+using AuthService.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+//Register context and services
+builder.Services.AddDbContext<AuthDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
+
 
 app.UseHttpsRedirection();
 
