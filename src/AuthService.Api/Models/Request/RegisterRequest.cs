df2b66fe164750b67fc97660d9f5254a268b943b
Carlos Quintero <72052457+CarlosQuinteroC@users.noreply.github.com>
2026-06-25 20:03:35 -0400

    feat: add RegisterRequest and RegisterResponse models for user registration

---
+++ src/AuthService.Api/Models/Request/RegisterRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace AuthService.Api.Models.Request;
+/// <summary>
+///  Defines the request model for registering a new user
+/// </summary>
+public class RegisterRequest
+{
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string Email { get; set; }
+    public required string Password { get; set; }
+    public string? PhoneNumber { get; set; }
+}
---
+++ src/AuthService.Api/Models/Response/RegisterResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace AuthService.Api.Models.Response;
+
+public class RegisterResponse
+{
+    public Guid Id { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string Email { get; set; }
+    public string? PhoneNumber { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
