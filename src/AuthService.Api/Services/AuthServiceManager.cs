08f002422d6c8435bb67ffe30a489b7de11b490f
Carlos Quintero <72052457+CarlosQuinteroC@users.noreply.github.com>
2026-06-26 21:51:21 -0400

    feat: enhance user registration flow with validation, email normalization, and role assignment

--- src/AuthService.Api/Services/AuthServiceManager.cs
+++ src/AuthService.Api/Services/AuthServiceManager.cs
@@ -3,6 +3,7 @@ using AuthService.Api.Entities;
 using AuthService.Api.Models.Request;
 using AuthService.Api.Models.Response;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace AuthService.Api.Services;
 
@@ -29,29 +30,81 @@ public class AuthServiceManager
     {
         _logger.LogInformation("---------------------------------------------------------------------------------------------------");
         _logger.LogInformation("Register user Flow Triggered");
-        // Validate Unique and valid email
-        if (request.Email == "")
+        // Validate request properties
+        if (string.IsNullOrWhiteSpace(request.FirstName)
+            || string.IsNullOrWhiteSpace(request.LastName) 
+            || string.IsNullOrWhiteSpace(request.Email)
+            || string.IsNullOrWhiteSpace(request.Password)
+            )
         {
-            _logger.LogInformation("Email Property cannot be empty");
-            throw new Exception("Email Property cannot be empty");
+            _logger.LogInformation("One of the required properties is missing");
+            throw new Exception("One of the required properties is missing");
         }
         // trim email
-        request.Email = request.Email.Trim();
-        var users = _context.Users.Where(u => u.Email == request.Email).FirstOrDefault();
-        if (users == null)
+        var normalizedEmail = request.Email.Trim().ToLowerInvariant();
+
+        var userEmailExists =  await _context.Users.Where(u => u.Email == normalizedEmail).AnyAsync();
+        if (userEmailExists)
         {
-            _logger.LogInformation("Users not found");
-            throw new Exception("Users not found");
+            _logger.LogInformation("User with email {email} already exist", normalizedEmail);
+            throw new Exception("User with email already exist");
         }
+        
+        // Create user in memory
+        var user = new User
+        {
+            Id = Guid.CreateVersion7(),
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            Email = normalizedEmail,
+            PasswordHash = "HashedPassword",
+            PhoneNumber = !string.IsNullOrWhiteSpace(request.PhoneNumber) ? request.PhoneNumber : null,
+        };
+        
+        // Hash the password
+        user.PasswordHash = _passwordHasher.HashPassword(user, request.Password);
+        
+        _context.Users.Add(user);
+        
+        // Get roles and find user role
+        var userRole  = await _context.Roles.Where(r => r.Name == "user").FirstOrDefaultAsync();
+        if (userRole  == null)
+        {
+            _logger.LogInformation("Role with Name User does not exist, creating role");
+            userRole = new Role
+            {
+                Id = Guid.CreateVersion7(),
+                Name = "user"
+            };
+            _context.Roles.Add(userRole);
+            _logger.LogInformation("Role {RoleName} created",  userRole.Name);
+        }
+        
+        
+        
+        // assign role to user
+        var userRoleAssignment = new UserRole
+        {
+            UserId = user.Id,
+            RoleId = userRole.Id
+        };
+        
+        _context.UserRoles.Add(userRoleAssignment);
+        
+        _logger.LogInformation("Creating user with name {FirstName} {LastName} and email {Email}", user.FirstName, user.LastName, user.Email);
+        
+        // Save in database
+        await _context.SaveChangesAsync();
 
         return new RegisterResponse
         {
-            Id = Guid.NewGuid(),
-            FirstName = request.FirstName,
-            LastName = request.LastName,
-            Email = request.Email,
-            PhoneNumber = request.PhoneNumber,
-            CreatedAt = DateTime.UtcNow
+            Id = user.Id,
+            Email = user.Email,
+            FirstName = user.FirstName,
+            LastName = user.LastName,
+            PhoneNumber = user.PhoneNumber,
+            CreatedAt = user.CreatedAt
         };
+
     }
 }
