2d0bfc61e7f6288289927419c68c71cf2cde5940
Carlos Quintero <72052457+CarlosQuinteroC@users.noreply.github.com>
2026-06-28 11:07:34 -0400

    feat: add Swagger and Scalar integration for API documentation and reference

--- src/AuthService.Api/Program.cs
+++ src/AuthService.Api/Program.cs
@@ -3,6 +3,7 @@ using AuthService.Api.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using AuthService.Api.Services;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,8 +16,18 @@ builder.Services.AddDbContext<AuthDbContext>(options =>
 // Register dependencies
 builder.Services.AddScoped<PasswordHasher<User>>();
 builder.Services.AddScoped<AuthServiceManager>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapSwagger("/openapi/{documentName}.json");
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.MapScalarApiReference();
+}
 
 app.UseHttpsRedirection();
 
